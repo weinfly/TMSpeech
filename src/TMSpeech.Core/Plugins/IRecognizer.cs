e37fde26312888b2341c69f06dc820a8cf949d9d
justfly <50286540+weinfly@users.noreply.github.com>
2025-12-02 13:16:05 +0800

    优化实时字幕，调整全部大写为只有首字母是大写的模式

--- src/TMSpeech.Core/Plugins/IRecognizer.cs
+++ src/TMSpeech.Core/Plugins/IRecognizer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,7 +14,48 @@ namespace TMSpeech.Core.Plugins
         public TextInfo(string text)
         {
             Time = DateTime.Now;
-            Text = text;
+            Text = ToTitleCase(text);
+        }
+        
+        /// <summary>
+        /// 将文本转换为标准的首字母大写格式
+        /// </summary>
+        /// <param name="text">输入文本</param>
+        /// <returns>首字母大写的文本</returns>
+        private string ToTitleCase(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+            
+            // 首先将所有字符转换为小写
+            text = text.ToLower();
+            
+            // 将第一个字符转换为大写
+            char[] chars = text.ToCharArray();
+            if (char.IsLetter(chars[0]))
+            {
+                chars[0] = char.ToUpper(chars[0]);
+            }
+            
+            // 将句子开头的字符转换为大写（以句号、问号、感叹号结尾的句子）
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if ((chars[i - 1] == '.' || chars[i - 1] == '?' || chars[i - 1] == '!' || chars[i - 1] == '。' || chars[i - 1] == '？' || chars[i - 1] == '！') && i + 1 < chars.Length)
+                {
+                    // 跳过空格
+                    int j = i;
+                    while (j < chars.Length && char.IsWhiteSpace(chars[j]))
+                    {
+                        j++;
+                    }
+                    if (j < chars.Length && char.IsLetter(chars[j]))
+                    {
+                        chars[j] = char.ToUpper(chars[j]);
+                    }
+                }
+            }
+            
+            return new string(chars);
         }
     }
 
